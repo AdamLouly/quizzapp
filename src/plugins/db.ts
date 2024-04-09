61939328c3ea7a3467d0a75167a7725f8a933bd2
achrefbenmehrez1 <45834286+achrefbenmehrez@users.noreply.github.com>
2024-04-09 19:56:58 +0100

    Update db.ts

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -1,14 +1,15 @@
 import fp from "fastify-plugin";
 import mongoose from "mongoose";
 
-export default fp(async (fastify, opts) => {
-  try {
+export default fp(
+  async (fastify, opts) => {
     mongoose.connection.on("connected", () => {
       console.log("Mongoose default connection is open");
     });
 
     mongoose.connection.on("error", (err: any) => {
-      console.log(`Mongoose default connection has occurred \n${err}`);
+      console.error("Mongoose default connection error:", err);
+      process.exit(1); // Consider a strategy for handling fatal errors
     });
 
     mongoose.connection.on("disconnected", () => {
@@ -23,9 +24,20 @@ export default fp(async (fastify, opts) => {
       process.exit(0);
     });
 
-    await mongoose.connect(process.env.MONGO_URI || "");
-    fastify.decorate("mongo", mongoose);
-  } catch (err) {
-    console.error(err);
-  }
-});
+    try {
+      // Use existing connection if already connected.
+      if (mongoose.connection.readyState !== 1) {
+        await mongoose.connect(process.env.MONGO_URI || "", {
+          serverSelectionTimeoutMS: 5000, // Set timeout for MongoDB connection
+        });
+      }
+      fastify.decorate("mongo", mongoose); // Make mongoose available throughout the Fastify instance.
+    } catch (err) {
+      console.error("MongoDB connection error:", err);
+      throw err; // Rethrow or handle as needed
+    }
+  },
+  {
+    name: "mongoose-db-connector",
+  },
+);

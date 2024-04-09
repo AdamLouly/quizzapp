114497ee54ef7571d683c9eb52302ae6b984d785
achrefbenmehrez1 <45834286+achrefbenmehrez@users.noreply.github.com>
2024-04-09 19:36:33 +0100

    test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,36 @@
-import { app } from "../src/server";
+"use strict";
+
+// Read the .env file.
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import("../src/app"));
 
 export default async (req, res) => {
-  await app.ready();
+  app.ready((err) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+
+    app.log.info(
+      "All routes loaded! Check your console for the route details.",
+    );
+
+    console.log(app.printRoutes());
+
+    app.log.info(
+      `Server listening on port ${Number(process.env.PORT ?? 3000)}`,
+    );
+  });
   app.server.emit("request", req, res);
 };

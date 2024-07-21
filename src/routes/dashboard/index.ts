d1bd0c1962eb29a1b86c6ce2a3f9acf2e322f90a
RealsmileApp <45834286+achrefbenmehrez@users.noreply.github.com>
2024-07-21 18:09:16 +0100

    Update index.ts

--- src/routes/dashboard/index.ts
+++ src/routes/dashboard/index.ts
@@ -1,9 +1,9 @@
 import { FastifyPluginAsync } from "fastify";
 import { Class } from "../../models/Class";
-import { Client } from "../../models/Client";
 import { Quiz } from "../../models/Quiz";
 import { QuizResult } from "../../models/QuizResult";
 import { User } from "../../models/User";
+import mongoose from "mongoose";
 
 const dashboardRoutes: FastifyPluginAsync = async (fastify, _opts) => {
   // Admin dashboard route
@@ -12,12 +12,38 @@ const dashboardRoutes: FastifyPluginAsync = async (fastify, _opts) => {
     { preValidation: [fastify.authenticate] },
     async (request, reply) => {
       try {
-        const totalUsers = await User.countDocuments({});
-        const userBreakdown = await User.aggregate([
-          { $group: { _id: "$role", count: { $sum: 1 } } },
-        ]);
+        const clientId = request.user.client; // Assuming the client's ID is stored in the user object
 
-        const userRegistrations = await User.aggregate([
+        // Count the number of students
+        const totalStudents = await User.countDocuments({
+          client: clientId,
+          role: "student",
+        });
+
+        // Count the total number of quizzes
+        const totalQuizzes = await Quiz.countDocuments({ client: clientId });
+
+        // Count the number of classes
+        const totalClasses = await Class.countDocuments({ client: clientId });
+
+        // Count the number of teachers
+        const totalTeachers = await User.countDocuments({
+          client: clientId,
+          role: "teacher",
+        });
+
+        // Daily quiz submissions
+        const dailySubmissions = await QuizResult.aggregate([
+          {
+            $lookup: {
+              from: "quizzes",
+              localField: "quizId",
+              foreignField: "_id",
+              as: "quiz",
+            },
+          },
+          { $unwind: "$quiz" },
+          { $match: { "quiz.client": new mongoose.Types.ObjectId(clientId) } },
           {
             $group: {
               _id: {
@@ -29,45 +55,25 @@ const dashboardRoutes: FastifyPluginAsync = async (fastify, _opts) => {
           { $sort: { _id: 1 } },
         ]);
 
-        const totalQuizzes = await Quiz.countDocuments({});
-        const quizzesByTeacher = await Quiz.aggregate([
-          { $group: { _id: "$createdBy", count: { $sum: 1 } } },
-        ]);
-
-        const totalClasses = await Class.countDocuments({});
-        const classesByTeacher = await Class.aggregate([
-          { $group: { _id: "$teacher", count: { $sum: 1 } } },
-        ]);
-
-        const clientData = await Client.countDocuments({});
-
-        const totalQuizAttempts = await QuizResult.countDocuments({});
-        const averageQuizScoresData = await QuizResult.aggregate([
+        // Monthly registrations
+        const monthlyRegistrations = await User.aggregate([
+          { $match: { client: new mongoose.Types.ObjectId(clientId) } },
           {
             $group: {
-              _id: null,
-              averageScore: {
-                $avg: { $multiply: [{ $divide: ["$score", 10] }, 100] },
-              },
+              _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+              count: { $sum: 1 },
             },
           },
+          { $sort: { _id: 1 } },
         ]);
-        const averageQuizScores =
-          averageQuizScoresData.length > 0
-            ? averageQuizScoresData[0].averageScore
-            : 0;
 
         reply.send({
-          totalUsers,
-          userBreakdown,
-          userRegistrations,
-          totalQuizzes,
-          quizzesByTeacher,
+          totalStudents,
           totalClasses,
-          classesByTeacher,
-          clientData,
-          totalQuizAttempts,
-          averageQuizScores,
+          totalTeachers,
+          totalQuizzes,
+          dailySubmissions,
+          monthlyRegistrations,
         });
       } catch (error) {
         reply
@@ -84,50 +90,62 @@ const dashboardRoutes: FastifyPluginAsync = async (fastify, _opts) => {
     async (request, reply) => {
       try {
         const teacherId = request.user._id;
-        const teacherClasses = await Class.find({ teacher: teacherId }).select(
-          "_id students",
-        );
+        const clientId = request.user.client;
 
+        const teacherClasses = await Class.find({
+          teacher: teacherId,
+          client: clientId,
+        }).select("_id students");
         const teacherClassIds = teacherClasses.map((cls) => cls._id);
+
         const totalStudents = teacherClasses.reduce(
           (acc, cls) => acc + cls.students.length,
           0,
         );
-
         const quizzesCreatedByTeacher = await Quiz.countDocuments({
           createdBy: teacherId,
+          client: clientId,
         });
-        const teacherQuizzes = await Quiz.find({ createdBy: teacherId });
 
+        // Corrected totalQuizAttempts calculation
+        const quizzesCreatedByTeacherIds = await Quiz.find({
+          createdBy: teacherId,
+          client: clientId,
+        }).select("_id");
+        const quizIds = quizzesCreatedByTeacherIds.map((quiz) => quiz._id);
         const totalQuizAttempts = await QuizResult.countDocuments({
-          quizId: { $in: teacherClassIds },
+          quizId: { $in: quizIds },
         });
 
         const quizzesCompletedByStudents = await QuizResult.countDocuments({
-          quizId: { $in: teacherClassIds },
-          score: { $gte: 0 }, // Assuming a score >= 0 means the quiz was completed
+          quizId: { $in: quizIds },
+          score: { $gte: 0 },
         });
 
-        const studentPerformance = await QuizResult.aggregate([
-          { $match: { quizId: { $in: teacherClassIds } } },
+        // Corrected dailySubmissions aggregation
+        const dailySubmissions = await QuizResult.aggregate([
           {
-            $group: {
-              _id: "$studentId",
-              averageScore: {
-                $avg: { $multiply: [{ $divide: ["$score", 10] }, 100] },
-              },
-              totalQuizzes: { $sum: 1 },
+            $match: {
+              quizId: { $in: quizIds },
             },
           },
+          {
+            $group: {
+              _id: {
+                $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
+              },
+              count: { $sum: 1 },
+            },
+          },
+          { $sort: { _id: 1 } },
         ]);
 
         reply.send({
           quizzesCreatedByTeacher,
-          teacherQuizzes,
           totalQuizAttempts,
           totalStudents,
           quizzesCompletedByStudents,
-          studentPerformance,
+          dailySubmissions,
         });
       } catch (error) {
         console.log(error);
